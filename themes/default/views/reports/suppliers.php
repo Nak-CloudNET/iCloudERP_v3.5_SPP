c584d17b990ae146a1a9b226a779f2cf2aa15389
Nak <khat.ratanak@cloudnet.com.kh>
2018-10-26 13:56:20 +0700

    Nak Upload SPP

---
+++ themes/default/views/reports/suppliers.php
@@ -0,0 +1,155 @@
+<style>
+    @media print {
+        thead tr th:last-child,
+        tbody tr td:last-child{
+            display: none;  /*hide column Action when click on print*/
+        }
+        .print-date-time {
+            display: block !important;
+        }
+    }
+</style>
+<script>
+    $(document).ready(function () {
+        var oTable = $('#CusData').dataTable({
+            "aaSorting": [[1, "asc"]],
+            "aLengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "<?= lang('all') ?>"]],
+            "iDisplayLength": <?= $Settings->rows_per_page ?>,
+            'bProcessing': true, 'bServerSide': true,
+            'sAjaxSource': '<?= site_url('reports/getSuppliers') ?>',
+            'fnServerData': function (sSource, aoData, fnCallback) {
+                aoData.push({
+                    "name": "<?= $this->security->get_csrf_token_name() ?>",
+                    "value": "<?= $this->security->get_csrf_hash() ?>"
+                });
+                $.ajax({'dataType': 'json', 'type': 'POST', 'url': sSource, 'data': aoData, 'success': fnCallback});
+            },
+            "aoColumns": [{"bSortable": false, "mRender": checkbox}, null, null, null, null, {
+                "mRender": null,
+                "bSearchable": false
+            }, {"mRender": currencyFormat, "bSearchable": false}, {
+                "mRender": currencyFormat,
+                "bSearchable": false
+            }, {"mRender": null}, {"bSortable": false}]
+        }).fnSetFilteringDelay().dtFilter([
+            {column_number: 1, filter_default_label: "[<?=lang('company');?>]", filter_type: "text", data: []},
+            {column_number: 2, filter_default_label: "[<?=lang('name');?>]", filter_type: "text", data: []},
+            {column_number: 3, filter_default_label: "[<?=lang('phone');?>]", filter_type: "text", data: []},
+            {column_number: 4, filter_default_label: "[<?=lang('email_address');?>]", filter_type: "text", data: []},
+        ], "footer");
+    });
+</script>
+<?php 
+
+    echo form_open('reports/suppliers_actions', 'id="action-form"');
+?>
+<div class="box">
+    <div class="box-header">
+        <h2 class="blue"><i class="fa-fw fa fa-users"></i><?= lang('suppliers'); ?></h2>
+        <div class="box-icon">
+            <ul class="btn-tasks">
+                <li class="dropdown"><a href="#" id="pdf" data-action="export_pdf"  class="tip" title="<?= lang('download_pdf') ?>"><i
+                            class="icon fa fa-file-pdf-o"></i></a></li>
+                <li class="dropdown"><a href="#" id="excel" data-action="export_excel"  class="tip" title="<?= lang('download_xls') ?>"><i
+                            class="icon fa fa-file-excel-o"></i></a></li>
+                <li class="dropdown"><a href="#" id="image" class="tip" title="<?= lang('save_image') ?>"><i
+                            class="icon fa fa-file-picture-o"></i></a></li>
+                <li class="dropdown">
+                    <a href="javascript:void(0)"><i class="icon fa fa-print" onclick="window.print()"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div style="display: none;">
+        <input type="hidden" name="form_action" value="" id="form_action"/>
+        <?= form_submit('performAction', 'performAction', 'id="action-form-submit"') ?>
+    </div>
+    <?= form_close() ?> 
+	<div class="box-content">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <p class="introtext"><?= lang('view_report_supplier'); ?></p>
+                <div class="report-header">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                            <div class="print-date-time" style="display: none">
+                                <br>
+                                <br>
+                                <span><?php echo date("F d, Y"); ?></span><br>
+                                <span><?php echo date("h:i a") ?></span>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                            <h3 class="text-center" style="font-size: 22px;">
+                                <?php echo $billers->company; ?>
+                            </h3>
+                            <h3 class="text-center" style="font-size: 25px;">Collections report</h3>
+                            <p class="text-center">
+                                <b><?php echo date("F d, Y"); ?></b>
+                            </p>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"></div>
+                    </div>
+                </div>
+                <div class="table-responsive">
+                    <table id="CusData" cellpadding="0" cellspacing="0" border="0"
+                           class="table table-bordered table-condensed table-hover table-striped reports-table">
+                        <thead>
+                        <tr class="primary">
+							<th style="min-width:30px; width: 30px; text-align: center;">
+                                <input class="checkbox checkth" type="checkbox" name="check"/>
+                            </th>
+                            <th><?= lang("company"); ?></th>
+                            <th><?= lang("name"); ?></th>
+                            <th><?= lang("phone"); ?></th>
+                            <th><?= lang("email_address"); ?></th>
+                            <th><?= lang("total_purchases"); ?></th>
+                            <th><?= lang("total_amount"); ?></th>
+                            <th><?= lang("paid"); ?></th>
+                            <th><?= lang("balance"); ?></th>
+                            <th style="width:85px;"><?= lang("actions"); ?></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td colspan="8" class="dataTables_empty"><?= lang('loading_data_from_server') ?></td>
+                        </tr>
+                        </tbody>
+                        <tfoot class="dtFilter">
+                        <tr class="active">
+							<th style="min-width:30px; width: 30px; text-align: center;">
+                                <input class="checkbox checkth" type="checkbox" name="check"/>
+                            </th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th class="text-center"><?= lang("total_purchases"); ?></th>
+                            <th class="text-center"><?= lang("total_amount"); ?></th>
+                            <th class="text-center"><?= lang("paid"); ?></th>
+                            <th class="text-center"><?= lang("balance"); ?></th>
+                            <th style="width:85px; text-align: center;"><?= lang("actions"); ?></th>
+                        </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="<?= $assets ?>js/html2canvas.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#image').click(function (event) {
+            event.preventDefault();
+            html2canvas($('.box'), {
+                onrendered: function (canvas) {
+                    var img = canvas.toDataURL()
+                    window.open(img);
+                }
+            });
+            return false;
+        });
+    });
+</script>
